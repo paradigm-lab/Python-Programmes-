f777ad05a952a4fd877237aee3093fc63977828f
collins <collinsboniface1000@gmail.com>
2021-11-28 21:11:23 +0200

    Adding the step by step execution of the program in the printing formart of the value in the program

--- square_root/soln4.py
+++ square_root/soln4.py
@@ -2,10 +2,13 @@
 
 num = float(input("Enter the number to compute the square root: "))
 approx = 0.5 * num
+print("Approx:", approx)
 better = 0.5 * (approx + num/approx)
+print("Better:", better)
 
 while (better != approx):
     approx = better
+    print("Approx:", approx)
     better = 0.5 * (approx + num / approx)
-
+    print("Better:", better)
 print("The square root of", num, "is", better, sep=" ")

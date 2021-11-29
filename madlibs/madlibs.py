1e1c266844c363d77fd0e94a7a866b622ef364f2
collins <collinsboniface1000@gmail.com>
2021-11-29 13:59:50 +0200

    Commenting the print statement and assineing the value into the youtuber variable and selecting one print to use in the madlibs project

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -1,11 +1,15 @@
 """
 String concatenation is the way of putting strings together.
 
-"""
 
-youtuber = ""
+
+youtuber = "Collins Boniface"
 
 # A few ways to do this 
 print("Subscribe to " + youtuber) # Concatenating the string using the + sign
 print("Subscribe to {}".format(youtuber)) # using the curly braces and calling the format method
 print(f"Subscribe to {youtuber}") # using an f string
+
+"""
+
+

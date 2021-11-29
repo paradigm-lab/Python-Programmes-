92969d4868ffc8a6dd4f3136d6415d24cdf47fef
collins <collinsboniface1000@gmail.com>
2021-11-29 13:57:24 +0200

    Adding the comment in the print statement to explaine what is happening when we are printing the string or concatenating the string

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -6,6 +6,6 @@ String concatenation is the way of putting strings together.
 youtuber = ""
 
 # A few ways to do this 
-print("Subscribe to " + youtuber)
-print("Subscribe to {}".format(youtuber))
-print(f"Subscribe to {youtuber}")
+print("Subscribe to " + youtuber) # Concatenating the string using the + sign
+print("Subscribe to {}".format(youtuber)) # using the curly braces and calling the format method
+print(f"Subscribe to {youtuber}") # using an f string

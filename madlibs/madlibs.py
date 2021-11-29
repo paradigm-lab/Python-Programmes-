1e388b22d68a6f3ace41b793df8650f561ee89df
collins <collinsboniface1000@gmail.com>
2021-11-29 13:52:25 +0200

    Creating a new madlibs folder for creating the madlibs game and making the madlibs file with same printing fomart and string concatination for the string

---
+++ madlibs/madlibs.py
@@ -0,0 +1,11 @@
+"""
+String concatenation is the way of putting strings together.
+
+"""
+
+youtuber = ""
+
+# A few ways to do this 
+print("Subscribe to " + youtuber)
+print("Subscribe to {}".format(youtuber))
+print(f"Subscribe to {youtuber}")

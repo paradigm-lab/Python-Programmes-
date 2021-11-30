5d8668d3ded85da95d8e1951ad344b35ba732ec7
collins <collinsboniface1000@gmail.com>
2021-11-30 17:52:48 +0200

    importing the player file and the class in the game file and making the instance of the TicTacToe class in the main and calling the play function in the main

--- TicTacToe/game.py
+++ TicTacToe/game.py
@@ -1,3 +1,6 @@
+from player import HumanPlayer, RandomComputerPlayer
+
+
 class TicTacToe:
     def __init__(self):
         self.board = [' ' for _ in range(9)]  # We will use a single list to represent 3x3
@@ -118,4 +121,8 @@ def play(game, x_player, o_player, print_game=True):
             print('It\'s a tie!')
 
 
-
+if __name__ == '__main__':
+    x_player = HumanPlayer('X')
+    o_player = RandomComputerPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, print_game=True)

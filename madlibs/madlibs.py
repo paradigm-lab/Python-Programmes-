b3d1f6c3438cf3030287eaa6c6ce62e0813b9bd0
collins <collinsboniface1000@gmail.com>
2021-12-13 22:47:47 +0200

    Fixed the format bug by removing the strings in the method arguments

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -18,7 +18,7 @@ verb2 = input("Verb: ")
 famous_person = input("Famous person: ")
 
 madlibs = "Computer programming is so {}! It makes me so excited all the time because \
-I love to {}. Stay hydrated and {} like you are {}!".format("adj", "verb1", "verb2", "famous_person")
+I love to {}. Stay hydrated and {} like you are {}!".format(adj, verb1, verb2, famous_person)
 
 print(madlibs)
 

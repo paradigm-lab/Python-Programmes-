73c74553f452e7b2ee74b416529e8eddbe63a37f
collins <collinsboniface1000@gmail.com>
2021-12-13 22:42:20 +0200

    Getting a runtime error

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -17,7 +17,7 @@ verb1 = input("Verb: ")
 verb2 = input("Verb: ")
 famous_person = input("Famous person: ")
 
-madlibs = f"Computer programming is so {adj}! It makes me so excited all the time because \
+madlibs = "Computer programming is so {adj}! It makes me so excited all the time because \
 I love to {verb1}. Stay hydrated and {verb2} like you are {}!".format(famous_person)
 
 print(madlibs)

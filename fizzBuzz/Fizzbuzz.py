0cafcf523501b7e7cec12cdb4e7841af67552834
collins <collinsboniface1000@gmail.com>
2021-12-25 20:58:36 +0200

    Added the fizz buzz pseudocode

--- fizzBuzz/Fizzbuzz.py
+++ fizzBuzz/Fizzbuzz.py
@@ -1,3 +1,16 @@
 # Fizz buzz is a group word game for children to teach them about division.
 
-# Player take turns to count increamentally, replacing any number divisible by three with the word "fizz" and any number divisible by five with the word "buzz" 
+# Player take turns to count increamentally, replacing any number divisible by three with the word "fizz" and any number divisible by five with the word "buzz"
+
+"""
+ Fizz buzz pseudocode:
+
+ number divisible by 3
+ returns 'fizz'
+
+ number divisible by 5
+ return 'buzz'
+
+"""
+
+

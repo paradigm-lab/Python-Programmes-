d5d3b2401db4f3bc8c3a147fd107e0522dca0511
collins <collinsboniface1000@gmail.com>
2021-12-25 20:55:37 +0200

    Creating a new folder for the fizz buzz game with file with the explanation about the fizz buzz game

---
+++ fizzBuzz/Fizzbuzz.py
@@ -0,0 +1,3 @@
+# Fizz buzz is a group word game for children to teach them about division.
+
+# Player take turns to count increamentally, replacing any number divisible by three with the word "fizz" and any number divisible by five with the word "buzz" 

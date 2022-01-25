68b094ca8eccfb19c450071f57809cb78ce62657
collins <collinsboniface1000@gmail.com>
2022-01-25 18:53:47 +0200

    Added the condition to check for the item in the list and use the print function to move to the next line and Add comment

--- Lists/list.py
+++ Lists/list.py
@@ -6,8 +6,21 @@ print(mylist)
 mylist2 = [5, True, "Apple", "Apple"]
 print(mylist2)
 
+print("\n")
+
 item = mylist2[-1]
 print(item)
 
+print("\n")
+
+# Iteration throught the list
 for i in  mylist2:
     print(i)
+
+print("\n")
+
+# Using the conditional statement to check for the item in the list
+if "Banana" in mylist:
+    print("Yes")
+else:
+    print("No")

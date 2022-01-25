1789d95b5166b470a822f41e77ccea3e88e5de71
collins <collinsboniface1000@gmail.com>
2022-01-25 18:50:41 +0200

    Accessing the list using the index and iteration through the list

--- Lists/list.py
+++ Lists/list.py
@@ -3,5 +3,11 @@
 mylist = ["Banana", "Cherry", "Apple"]
 print(mylist)
 
-mylist2 = list()
+mylist2 = [5, True, "Apple", "Apple"]
 print(mylist2)
+
+item = mylist2[-1]
+print(item)
+
+for i in  mylist2:
+    print(i)

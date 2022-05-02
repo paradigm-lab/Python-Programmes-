fbbdc51f284c205056db44271082f4b4aa8d5249
collins <collinsboniface1000@gmail.com>
2022-05-02 20:02:55 +0300

    Explored more about the function definition and function calling

--- intro/functions.py
+++ intro/functions.py
@@ -1,1 +1,17 @@
+# Function is a block of code which only runs when it's called
+# You can pass data, known as parameter or without parameter
+# A function can return data as a result
 
+# A fucntion is defined using the def keyword
+
+calculation_to_seconds = 24 * 60 * 60
+name_of_unit = "seconds"
+
+# Function Definition
+def days_to_units(): 
+	print(f"20 days are {20 * calculation_to_seconds} {name_of_unit}")
+	print("All good!")
+
+
+# Function Calling
+days_to_units()

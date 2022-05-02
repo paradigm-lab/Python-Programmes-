d24e5fa97bfbb539f5383a7a7a2992d15722b886
collins <collinsboniface1000@gmail.com>
2022-05-02 20:14:35 +0300

    Implememted the function parameter or argument

--- intro/functions.py
+++ intro/functions.py
@@ -1,5 +1,6 @@
 # Function is a block of code which only runs when it's called
 # You can pass data, known as parameter or without parameter
+# Parameter are also called Arguments
 # A function can return data as a result
 
 # A fucntion is defined using the def keyword
@@ -7,11 +8,12 @@
 calculation_to_seconds = 24 * 60 * 60
 name_of_unit = "seconds"
 
-# Function Definition
-def days_to_units(): 
-	print(f"20 days are {20 * calculation_to_seconds} {name_of_unit}")
-	print("All good!")
 
+# Function Definition
+def days_to_units(num_of_days, message): 
+	print(f"{num_of_days} days are {num_of_days * calculation_to_seconds} {name_of_unit}")
+	print(message)
 
 # Function Calling
-days_to_units()
+days_to_units(20, "Awesome")
+days_to_units(35, "Great")

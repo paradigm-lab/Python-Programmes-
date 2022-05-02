a4af7c7de0661990f29327b2ca0216ac1a8a377c
collins <collinsboniface1000@gmail.com>
2022-05-02 19:40:13 +0300

    Explored about variables and naming convention of variables in python

--- intro/variables.py
+++ intro/variables.py
@@ -1,1 +1,13 @@
+# Variables are container for storing values.
+# Python is dynamically typed.
+# Naming Convention is a convention (generally agreed schemem) for naming thins.
+# 		Use Lowercase with words separed by underscores
 
+# Reserved keyword in Python?
+	# Reserved words (also called keywords) are defined with predefined meaning and syntax in the language. eg: and, as, break, class, expect, finally
+
+calculation_to_seconds = 24 * 60 * 60
+print(f"20 days are {20 * calculation_to_seconds} seconds")
+print(f"20 days are {35 * calculation_to_seconds} seconds")
+print(f"50 days are {50 * calculation_to_seconds} seconds")
+print(f"110 days are {110 * calculation_to_seconds} seconds")

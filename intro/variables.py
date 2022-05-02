c91886176f2c7e827e478eb27889e148325ba48f
collins <collinsboniface1000@gmail.com>
2022-05-02 19:45:55 +0300

    Giving a variable name for the unit

--- intro/variables.py
+++ intro/variables.py
@@ -7,7 +7,9 @@
 	# Reserved words (also called keywords) are defined with predefined meaning and syntax in the language. eg: and, as, break, class, expect, finally
 
 calculation_to_seconds = 24 * 60 * 60
-print(f"20 days are {20 * calculation_to_seconds} seconds")
-print(f"20 days are {35 * calculation_to_seconds} seconds")
-print(f"50 days are {50 * calculation_to_seconds} seconds")
-print(f"110 days are {110 * calculation_to_seconds} seconds")
+name_of_unit = "seconds"
+
+print(f"20 days are {20 * calculation_to_seconds} {name_of_unit}")
+print(f"35 days are {35 * calculation_to_seconds} {name_of_unit}")
+print(f"50 days are {50 * calculation_to_seconds} {name_of_unit}")
+print(f"110 days are {110 * calculation_to_seconds} {name_of_unit}")

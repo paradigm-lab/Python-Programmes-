3fdcebf5536dc6d7f5101f4795064ffaeaaa0682
collins <collinsboniface1000@gmail.com>
2022-05-02 19:22:39 +0300

    Giving the description of the Data types and Arithmetic Operator

--- intro/main.py
+++ intro/main.py
@@ -1,1 +1,11 @@
-print(1)
+# Python Data types
+	# Text Type: String(str) -> using single and double quote for the Strings.
+	# Number Type: Integer(int) whole number, positive or negative, without decimals.
+        #	       Floating Point Number(Float) number, Positive or negative containing 1 or more decimals.
+
+# Arithmetic Operators are used with numeric values for mathematical operations.
+# Operators are used to perform operations.
+
+
+# Getting the minutes for within 20 days
+print("20 days are " + str(20 * 24 * 60) + " minutes")

afc8ceea796e974d30fc955abef2acae6b103ae7
collins <collinsboniface1000@gmail.com>
2022-05-05 12:11:10 +0300

    Created a new file to explore more about the input function in python

---
+++ intro/input.py
@@ -0,0 +1,13 @@
+
+calculation_to_seconds = 24 * 60 * 60
+name_of_unit = "seconds"
+
+
+# Function Definition
+def days_to_units(num_of_days):
+	print(f"{num_of_days} days are {num_of_days * calculation_to_seconds} {name_of_unit}")
+
+# To ask the user for a input 
+# Built-In F(x) are provided by Python language itself
+input()
+

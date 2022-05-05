caafa43ff8082d4e5bc389f1d1d6b671dfa1e153
collins <collinsboniface1000@gmail.com>
2022-05-05 12:28:32 +0300

    Getting the user input and the days_to_units function to calculate for the seconds

--- intro/input.py
+++ intro/input.py
@@ -5,9 +5,15 @@ name_of_unit = "seconds"
 
 # Function Definition
 def days_to_units(num_of_days):
-	print(f"{num_of_days} days are {num_of_days * calculation_to_seconds} {name_of_unit}")
+	return f"{num_of_days} days are {num_of_days * calculation_to_seconds} {name_of_unit}"
+
 
 # To ask the user for a input 
 # Built-In F(x) are provided by Python language itself
 user_input = input("Hey user, enter a number of days and I will convert it to hours! \n")
-print(user_input)
+
+# Casting 
+user_input_number = int(user_input)
+
+calculated_value = days_to_units(user_input_number)
+print(calculated_value)

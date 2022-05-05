392b920ee76545dd126c6ce7dfa8040fc9455608
collins <collinsboniface1000@gmail.com>
2022-05-05 12:38:25 +0300

    Using the condition statement flow to validate the user input

--- intro/input.py
+++ intro/input.py
@@ -5,7 +5,10 @@ name_of_unit = "seconds"
 
 # Function Definition
 def days_to_units(num_of_days):
-	return f"{num_of_days} days are {num_of_days * calculation_to_seconds} {name_of_unit}"
+    if num_of_days >= 0:
+        return f"{num_of_days} days are {num_of_days * calculation_to_seconds} {name_of_unit}"
+    else:
+        return "You entered a negative value, so no conversion for you!"
 
 
 # To ask the user for a input 

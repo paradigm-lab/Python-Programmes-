ec2d30ec322cb9bdd23c64513fac0d1d08166068
collins <collinsboniface1000@gmail.com>
2022-05-07 19:21:40 +0300

    Explored more about the Basic of the sets operation and update the conversion program to use the set to get the unque values from the user input

--- intro/input2.py
+++ intro/input2.py
@@ -3,17 +3,27 @@ Data Types: Integers, Strings, Boolean, Float
 
 Lists -> To store multiple items in a single variable
       -> A list can contain different data types
+      -> Lists are represented using the square brackets []
 
       Basic List Operations:
       -> Create a list ->  Using the square brackets
       -> Add an item to the list -> Using the append() method 
-      -> Remove an item from the list
+      -> Remove an item from the list -> using the remove method by specifing the actual item within the list
       -> Change items in the list
       -> Access item of the list -> Using the index number eg: fruits[0]
 
 Sets -> Built-in data type of python a
      -> as with Lists, used to store multiple items of data
      -> Does NOT allow duplicate values
+     -> Sets are represented using the curly braces { ... }
+
+      Basic Sets Operations:
+      -> Create a list ->  Using the curly braces { ... }
+      -> Add an item to the list -> Using the add method
+      -> Remove an item from the list -> using the remove method by specifing the actual item within the set
+      -> Change items in the list
+      -> Access item of the list -> Using the for loop and not the !index
+                                 -> When printing the item they are not ordered same as the list 
 
 For loop
       -> Is used for iterating over a sequence (like a list)
@@ -54,11 +64,20 @@ while user_input != "exit":
     # To ask the user for a input 
     # Built-In F(x) are provided by Python language itself 
     user_input = input("Hey user, enter a number of days as a comma separted list and I will convert it to seconds \n")
-    print(type(user_input.split(","))) 
-    print(user_input.split(", ")) 
+
+    list_of_days = user_input.split(", ")
+   
+
+    '''
+    print(list_of_days)
+    print(set(list_of_days))
+    print(type(list_of_days))
+    print(type(set(list_of_days)))
+    '''
+
 
     # The split() will splits a string into a list
     # By default separator is any whitespace it can be overrided
     # Looping through a list and getting one element at a time and that is why we don't use the index
-    for num_of_days_element in user_input.split(", "): 
+    for num_of_days_element in set(list_of_days):
         validate_and_execute()
